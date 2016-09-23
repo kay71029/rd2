55d43b45cec7ca8f23f4fabdba2f1ce20a723170
kay71029 <kay71029@gmail.com>
2016-09-23 09:19:56 +0800

    新增開戶功能

--- BankSystem/API/addUser.php
+++ BankSystem/API/addUser.php
@@ -3,8 +3,11 @@ header('Content-Type: application/json; charset=utf-8');
 date_default_timezone_set("Asia/Taipei");
 require('MySqlConnect.php');
 $id = $_POST["id"];
+$date = date("Y-m-d");
+$time = date("H:i:s");
+
 checkUser($id);
-addUser($id);
+addUser($id ,$date , $time);
 function checkUser($id)
 {
     if ($id == null || !preg_match("/^([A-Za-z0-9]+)$/",$id)) {
@@ -12,7 +15,7 @@ function checkUser($id)
        exit();
     }
 }
-function addUser($id)
+function addUser($id ,$date , $time)
 {
     $db = DB();
     $sql = "SELECT `id` FROM `user` WHERE `id`= '$id'";
@@ -31,5 +34,16 @@ function addUser($id)
     $result = $db->prepare($sql);
     $result->bindParam(':id', $id);
     $result->execute();
-    echo json_encode(array('id' => $id, 'message' => "成功"),JSON_UNESCAPED_UNICODE);
+    $sql = "INSERT INTO `record`(`id`, `date`, `time`, `remark`, `type`, `money`, `blance`, `newBlance`) 
+        VALUES (:id, :date, :time, '樂透遊戲--帳戶開戶', '入款' ,'100' , '0' , '100')";
+    $result = $db->prepare($sql);
+    $result->bindParam(':id', $id);
+    $result->bindParam(':date', $date);
+    $result->bindParam(':time', $time);
+    $result->execute();
+    $sql = "UPDATE `user` SET `account` = 100 WHERE `id` = :id";
+    $result = $db->prepare($sql);
+    $result->bindParam(':id', $id);
+    $result->execute();
+    echo json_encode(array('id' => $id, 'message' => "開戶成功"),JSON_UNESCAPED_UNICODE);
 }
